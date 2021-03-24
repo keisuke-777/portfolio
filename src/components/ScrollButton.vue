6dda24f0a60dd35d467b9e55b26aa52211152450
keisuke <keisuke7771120@me.com>
2021-03-24 13:51:35 +0900

    ボタンのホバー処理を追加

--- src/components/ScrollButton.vue
+++ src/components/ScrollButton.vue
@@ -1,22 +1,59 @@
 <template>
   <div>
-    <button class="nav-button">About</button>
+    <button class="sc-button">About</button>
     <!-- 自己紹介的なことを書く -->
-    <button class="nav-button">Product</button>
+    <button class="sc-button">Product</button>
     <!-- いわゆる製作物を書く -->
-    <button class="nav-button">Activity</button>
+    <button class="sc-button">Activity</button>
     <!-- 課外活動とか表彰経験書く？？ -->
-    <button class="nav-button">Contact</button>
+    <button class="sc-button">Contact</button>
     <!-- アカウントリスト -->
   </div>
 </template>
 
 <style>
-.nav-button {
+.sc-button {
   padding: 80px;
   border: none;
   border-radius: 5px;
   margin: 10px;
   outline: none;
+}
+.sc-button:hover {
+  cursor: pointer;
+  text-decoration: none;
+  -webkit-animation: sc-button 0.5s both;
+  -moz-animation: sc-button 0.5s both;
+  animation: sc-button 0.5s both;
+}
+@-webkit-keyframes sc-button {
+  0% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+  100% {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+}
+@-moz-keyframes sc-button {
+  0% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+  100% {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+}
+@keyframes sc-button {
+  0% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+  100% {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
 }
 </style>
